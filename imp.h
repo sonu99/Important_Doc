69cb5f16c8b9c7672cd4b70706a5848941e53472
sonu99 <sonu99@users.noreply.github.com>
2016-04-07 15:27:01 +0530

    Create imp.h

---
+++ imp.h
@@ -0,0 +1,71 @@
+C programming
+Linux system programming 
+linux device driver 
+kernel programming
+Linux internals
+x86 Archetecture
+ARM Archetecture(Cortex)
+Hands on basic interface of 8051.
+C++ programming
+
+
+Linux Device Driver...Topics..
+--------------------------------
+What is Kernel
+Linux System Architecture
+Linux Software Architecture
+Basic Kernel Services
+Linux Kernel Code
+What is a Device Driver
+Classes of Devices
+Device Driver Classification
+Concept of a Module
+Fundamental Concepts
+Kernel Module Vs Application
+Namespace
+Major & Minor Numbers
+Reserved Major Numbers
+Module Parameters
+Loading/Unloading Modules
+Current Process Information
+Kernel Memory Allocations
+Driver Entry Points
+Driver Switch Tables
+Module Init & Exit
+Device Registration
+Character Device Drivers
+File Structure
+File Operations Structure
+Driver-User Data Transfer
+Driver-Kernel Communication
+Driver-Device Communication
+Device File Creation
+Device File Control Operations
+Ioctl command numbers
+Capabilities & Restricted Operations
+Driver Usage Count
+Kernel Synchronization Mechanisms
+Introduction to Race Conditions
+Sources of Race Conditions
+Preemption – User & Kernel
+Preemption APIs
+Interrupt Handling APIs
+Semaphores
+Binary & Counting Semaphores
+Reader Writer Semaphores
+Semaphore APIs
+Mutexes
+Spinlocks
+Spinlock APIs
+Atomic Operations
+Seqlocks
+Seqlock APIs
+Completions
+Deadlock – Sources
+Deadlock Prevention
+Waitqueues
+Rules for Sleeping
+Waitqueue APIs
+Linux Kernel Tree
+Linux Source Code
+Linux Kernel Configuration
