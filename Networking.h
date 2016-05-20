915c7beaa4337a73518e1354f876381b37a446f0
sonu99 <sonu99@users.noreply.github.com>
2016-05-20 18:51:06 +0530

    Create Networking.h

---
+++ Networking.h
@@ -0,0 +1,27 @@
+Q>> why OSI model.
+Q>> OSI layear and it's servies.
+Q>> TCP/IP layear and it's servies.
+Q>> how device get MAC address.
+Q>> if ARP request is broadcast then where we put destination MAC address FFFFFFF.
+Q>> Draw back of setting static and manual ip address
+ans: it leads to ip address wastage and mobility of node(device)
+Q>> where we use static ip address assigment.
+Ans:1> server which is accessing from remote because if it is changing the how will you connect.
+    2>if network addmin want to track trafic from particular system then it should run at static ip.
+Q>> what is RARP.
+Ans: reverse of ARP ..it will supply MAC address and get IP address from RARP server.
+Q>> why RARP.
+Ans: since diskless workstation are having less memory, they can't store the entire OS to boot, so after booting it will connect with RARP
+server and get the ip address.
+Q>> what is BOOTP.
+Ans:-BOOTP is used by diskless workstation, which gather the information(IP address of system) from BOOTP server. 
+Q>>how it work.
+Ans:- it's work in similar way of DHCP but diffrence is it allocate static IP address and table is similar to DHCP.
+Q>> DHCP server IP allocation.
+Ans:- using DORA process...it will assign the ip address for some time and befor time get over 
+Q>> how you will come to know collision occure.what is CSMA/CD.
+Ans if voltage level is raise then we can say collision occure(sudden change in electrical signal).if collision occure in medium that time
+device should backoff mean it shouldn't transmit data to medium.it will wait untiil route is clear.time is calculated by backoff
+algorithm and CSMA/CD uses backoff algorithm to calculate waiting time.
+Q>> how router select best path for any packet.
+Ans: Routing protocol running in the router which help to build the routing table and exchange the information.
