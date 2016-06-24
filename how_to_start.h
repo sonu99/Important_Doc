28a6eed954f48a2e8d0420500b88fb2ad42c4e5e
sonu99 <sonu99@users.noreply.github.com>
2016-06-24 11:43:59 +0530

    Create how_to_start.h

---
+++ how_to_start.h
@@ -0,0 +1,7 @@
+1> Kernel module programming.
+2> linux sytem programming.
+3> unix shell scripting
+4> device driver(Basic to USB)
+5> device driver (i2c based LCD, LED, temp)
+6> C
+7> embedded system 
