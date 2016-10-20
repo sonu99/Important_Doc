7106f19062dfe5482279fe077f9e07aa8f2966a1
sonu99 <sonu99@users.noreply.github.com>
2016-10-20 11:13:15 +0530

    Update Important_logic.h

--- Important_logic.h
+++ Important_logic.h
@@ -6,14 +6,12 @@
 
 int main() {
     int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    int i, j;
-
+    int array1[9];
+    int i, j, k=0;
     for (j = 0; j < 3; ++j) {
         for (i = 0; i < 3; ++i) {
-            printf("%d ", array[j + i * 3]);
+            array1[k++] = array[j + i * 3];
         }
-        printf("\n");
     }
 }
 3> http://www.dailyfreecode.com/code/sorting-link-list-2425.aspx
