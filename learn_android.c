eb53f244c80d22292602e8c07a2da26ff7a396bf
sonu99 <sonu99@users.noreply.github.com>
2017-04-12 23:34:18 +0530

    Update learn_android.c

--- learn_android.c
+++ learn_android.c
@@ -1,1 +1,8 @@
 https://www.edureka.co/blog/beginners-guide-android-architecture/
+
+
+bild andriod for pi use this link
+https://github.com/peyo-hd/local_manifests
+https://source.android.com/source/downloading
+
+---------------------history-------------------------
